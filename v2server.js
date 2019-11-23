0440e19a65daea8b91d5fa080a34fc5a69691c46
Gregor Wolf <gregor.wolf@gmail.com>
2019-11-24 00:40:17 +0100

    Add OData V2 Proxy and Fiori Elements UI

---
+++ v2server.js
@@ -0,0 +1,37 @@
+// load modules
+const express = require('express');
+const cds = require('@sap/cds');
+const proxy = require('@sap/cds-odata-v2-adapter-proxy');
+
+// config
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 4004;
+
+(async () => {
+  // create new app
+  const app = express();
+  app.use('/', express.static('appadmin/'))
+
+  app.get('/', function(req, res) {
+    res.redirect('/webapp/index.html')
+  });
+
+  // serve odata v4
+  await cds
+    .connect('db') // ensure database is connected!
+    .serve('all')
+    .in(app);
+
+  // serve odata v2
+  process.env.XS_APP_LOG_LEVEL = 'none'; // suppress debug logs
+  app.use(proxy({
+    // app
+    path: 'v2',
+    // target
+    port: port
+  }));
+
+  // start server
+  const server = app.listen(port, host, () => console.info(`app is listing at ${port}`));
+  server.on('error', error => console.error(error.stack));
+})();
